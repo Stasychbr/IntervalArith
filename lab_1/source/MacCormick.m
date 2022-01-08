87a74d1a41f52ef9e2bd298c2b687d15e5c4390f
Станислав <stasychbr@mail.ru>
2022-01-08 18:32:47 +0300

    structure change

--- lab_1/source/MacCormick.m
+++ lab_1/source/MacCormick.m
@@ -1,6 +1,6 @@
 function Y = MacCormick(x, y)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-Y = sin(x + y) + sqr(x - y) - 1.5 * x + 2.5 * y + 1;
+Y = sin(x + y) + (x - y) .* (x - y) - 1.5 * x + 2.5 * y + 1;
 end
 
